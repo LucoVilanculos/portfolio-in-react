32fd54aeec8a97af0bc45fc46347f9e046d535a4
LucoVilanculos <angelatave8@gmail.com>
2025-06-09 13:18:17 +0200

    chore: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,10 +9,10 @@ export const Footer = ({ name }: FooterProps) => {
   return (
     <footer className="bg-gradient-to-br from-cyan-950 via-cyan-800 to-blue-900 text-white py-6 px-4 flex flex-col items-center text-center">
       <div className="flex gap-4 mb-2">
-        <FacebookLogoIcon size={28} className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
-        <InstagramLogoIcon size={28} className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
-        <LinkedinLogoIcon size={28} className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
-        <GithubLogoIcon size={28} className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
+        <FacebookLogoIcon size={28} href="https://www.facebook.com/luisisaura.vilanculos/" className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
+        <InstagramLogoIcon size={28} href = "https://www.instagram.com/sr.lucoooo/" className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
+        <LinkedinLogoIcon size={28} href = "https://www.linkedin.com/in/luco-vilanculos-5b551432a" className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
+        <GithubLogoIcon size={28} href = "https://github.com/LucoVilanculos" className="hover:scale-110 transition-transform duration-300 cursor-pointer" />
       </div>
       <p className="text-sm">
         &copy; {new Date().getFullYear()} all rights included and reserved for <strong>{name}</strong>
